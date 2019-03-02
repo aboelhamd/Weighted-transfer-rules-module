7dd3844074c878b0384e355d1c55830a9febed21
aboelhamd <aboelhamd@aboelhamd-HP-ProBook-450-G5>
2019-03-02 03:43:57 +0200

    Added helper scripts

---
+++ put-rules-ids.py
@@ -0,0 +1,19 @@
+import sys
+
+if (len(sys.argv) < 3) :
+	print('Usage: put-ids.py <original transfer file name> <new transfer file name>');
+	sys.exit(-1)
+
+oldfile = open(sys.argv[1], 'r')
+newfile = open(sys.argv[2], 'w')
+
+id = 0
+
+for line in oldfile :
+	if (line.find("rule ") > -1) :
+		id = id + 1
+	line = line.replace("rule ","rule id=\""+str(id)+"\" ")
+	newfile.write("%s\n" % line)
+
+oldfile.close()
+newfile.close()
